ba2352e69a5fc96688708ab94846687b1e074008
XtomiSN <90341045+XtomiSN@users.noreply.github.com>
2021-12-20 17:17:01 +0000

    `something`

--- MusicAndVideo/play.py
+++ MusicAndVideo/play.py
@@ -13,9 +13,9 @@ from pytgcalls.types.input_stream.quality import (
 from youtubesearchpython import VideosSearch
 
 from config import HNDLR, bot, call_py
-from MusicAndVideo.helpers.queues import QUEUE, add_to_queue, get_queue
-from MusicAndVideo.helpers.other.generator.thumbnail import gen_thumb
 from MusicAndVideo.helpers.other.generator.chattitle import CHAT_TITLE
+from MusicAndVideo.helpers.other.generator.thumbnail import gen_thumb
+from MusicAndVideo.helpers.queues import QUEUE, add_to_queue, get_queue
 
 
 # music player
