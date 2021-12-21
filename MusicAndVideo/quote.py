3acc64bd87b16b6913a20366d4ba0de019c4854f
Tomi Setiawan <90341045+XtomiSN@users.noreply.github.com>
2021-12-21 13:09:29 +0700

    Update quote.py

--- MusicAndVideo/quote.py
+++ MusicAndVideo/quote.py
@@ -9,7 +9,7 @@ from Python_ARQ import ARQ
 from config import HNDLR
 from MusicAndVideo.helpers.merrors import capture_err
 
-ARQ_API_KEY = "QFOTZM-GSZUFY-CHGHRX-TDEHOZ-ARQ"
+ARQ_API_KEY = "HMPXNS-BDPCCB-UJKRPU-OQADHG-ARQ"
 aiohttpsession = aiohttp.ClientSession()
 arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
 
